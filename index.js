2474fcdb19047e5b397cbe98917469da279f2ddf
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-15 18:54:19 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ import './style.css';
 
 // Writge Javascript code!
 var dino=document.getElementById("dino");
-function jumpdino()
+function jumpdino(e)
 {
+  if(e.key=="ArrowUp")
+  {
   dino.classList.add("jump");  
   function removeclass()
   {
 dino.classList.remove("jump");
   }  
+  console.log(e.key);
   setTimeout(removeclass,500);
-}
+}}
 addEventListener("keydown",jumpdino)
